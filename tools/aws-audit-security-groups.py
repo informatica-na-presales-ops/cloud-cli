fb43c691b5866b3664baaab4911834990dbfc220
William Jackson <william@subtlecoolness.com>
2020-03-11 14:44:56 -0500

    Add a tool to search AWS security groups for an IP range

---
+++ tools/aws-audit-security-groups.py
@@ -0,0 +1,56 @@
+"""
+This tool will check all security groups in all regions for an AWS account, looking for inbound rules for 0.0.0.0/0.
+If you want to look for a different range, set the TARGET_RANGE environment variable.
+"""
+
+import boto3
+import botocore.exceptions
+import logging
+import os
+import sys
+
+log = logging.getLogger(__name__)
+
+
+class Settings:
+    def __init__(self):
+        self.log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
+        self.log_level = os.getenv('LOG_LEVEL', 'INFO')
+        self.target_range = os.getenv('TARGET_RANGE', '0.0.0.0/0')
+        self.version = os.getenv('IMAGE_VERSION', 'unknown')
+
+
+def get_available_regions():
+    session = boto3.session.Session()
+    return session.get_available_regions('ec2')
+
+
+def sg_has_range(sg, target_range: str) -> bool:
+    for perm in sg.ip_permissions:
+        for ip_range in perm.get('IpRanges'):
+            if ip_range.get('CidrIp') == target_range:
+                return True
+    return False
+
+
+def main():
+    settings = Settings()
+    logging.basicConfig(format=settings.log_format, level='DEBUG', stream=sys.stdout)
+    log.debug(f'audit-security-groups {settings.version}')
+    if not settings.log_level == 'DEBUG':
+        log.debug(f'Changing log level to {settings.log_level}')
+    logging.getLogger().setLevel(settings.log_level)
+
+    log.info(f'Searching for inbound rules for {settings.target_range}')
+    for region in get_available_regions():
+        ec2 = boto3.resource('ec2', region_name=region)
+        try:
+            for sg in ec2.security_groups.all():
+                if sg_has_range(sg, settings.target_range):
+                    log.info(f'{region} {sg.id}')
+        except botocore.exceptions.ClientError:
+            log.warning(f'Skipping region {region}')
+
+
+if __name__ == '__main__':
+    main()
