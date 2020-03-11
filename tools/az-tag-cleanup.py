cc0c05da537ab6ff6fd719e1869548a4625e7830
William Jackson <william@subtlecoolness.com>
2020-03-11 14:48:03 -0500

    Add a tool to assist in Azure tag cleanup

---
+++ tools/az-tag-cleanup.py
@@ -0,0 +1,34 @@
+import azure.common.credentials
+import azure.mgmt.resource
+import azure.mgmt.subscription
+import os
+
+
+def get_subscriptions(credentials: azure.common.credentials.ServicePrincipalCredentials):
+    with azure.mgmt.subscription.SubscriptionClient(credentials) as client:
+        yield from client.subscriptions.list()
+
+
+def check_tags(resource):
+    print(resource.id)
+    if resource.tags is None:
+        resource.tags = {}
+    for k in resource.tags.keys():
+        kl = k.lower()
+        # if 'bus' in kl and not kl.startswith('b'):
+        if kl == 'applicatioenv':
+            print(repr(k))
+
+
+def main():
+    credentials = azure.common.credentials.ServicePrincipalCredentials(client_id=os.getenv('AZ_CLIENT_ID'),
+                                                                       secret=os.getenv('AZ_SECRET'),
+                                                                       tenant=os.getenv('AZ_TENANT'))
+    for sub in get_subscriptions(credentials):
+        with azure.mgmt.resource.resources.ResourceManagementClient(credentials, sub.subscription_id) as client:
+            for r in client.resources.list():
+                check_tags(r)
+
+
+if __name__ == '__main__':
+    main()
